c2b4c7ff253ee664e3f40dcbbf41e1b65576443a
JasonDanny <xiong__dan@126.com>
2018-07-23 16:36:43 +0800

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Danny on 2017/10/22.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.printf("hello,Danny,welcome to java first program");
+    }
+}
